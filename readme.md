9a8db1cc199deaa4b85ee21eab7f3c4fcaf53888
David Luecke <daff@neyeon.com>
2012-11-28 23:25:24 -0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# can.compile
+
+NodeJS module that compiles CanJS EJS and Mustache views into a single JavaScript file for lightning fast
+production apps.
+
+## Command line
+
+## Grunt task
---
+++ tasks/cancompile.js
@@ -0,0 +1,7 @@
+var compiler = require('../lib');
+
+module.exports = function (grunt) {
+	grunt.registerMultiTask('cancompile', 'Compile CanJS EJS and Mustache views', function () {
+
+	});
+}
